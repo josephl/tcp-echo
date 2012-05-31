59202f8cee161cc3141b63d5e212a4fc104230ff
Joseph Lee <josephl@cs.pdx.edu>
2012-05-31 02:22:11 -0700

    Added VERSION, DAYTIME, RANDOM funcionality
    Signed-off-by: Joseph Lee <josephl@cs.pdx.edu>

--- Makefile
+++ Makefile
@@ -5,13 +5,10 @@
 
 
 CC = gcc
-CFLAGS = -g -Wall
-ALL = echoclient echoserver
+CFLAGS = -g -Wall -gstabs
+ALL = echoserver
 
 all: $(ALL)
-
-echoclient: echoclient.c
-	$(CC) $(CFLAGS) -o echoclient echoclient.c
 
 echoserver: echoserver.c
 	$(CC) $(CFLAGS) -o echoserver echoserver.c
--- echoserver.c
+++ echoserver.c
@@ -19,6 +19,11 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define L_PORT 11466
 
 void sigchild(int sig) {
     pid_t pid = wait(0);
@@ -30,12 +35,17 @@ int main() {
     int r;
     struct sockaddr_in addr;
     int s;
+    int i;
+    time_t now;
+    struct tm *tp;
+    char buf[512];
+
     void (*sr)(int) = signal(SIGCHLD, sigchild);
     assert(sr != SIG_ERR);
     s = socket(AF_INET, SOCK_STREAM, 0);
     assert(s != -1);
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(2041);
+    addr.sin_port = htons(L_PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
     r = bind(s, (struct sockaddr *)&addr, sizeof(addr));
     assert(r != -1);
@@ -53,13 +63,41 @@ int main() {
             pid = getpid();
             printf("Child %d\n", pid);
             while(1) {
-                char buf[512];
+                bzero((void *)buf, 512);
                 r = read(s0, buf, 512);
-                assert(r != -1);
-                printf("Child %d: read %d bytes\n", pid, r);
-                if (r == 0)
-                    break;
-                r = write(s0, buf, r);
+                /* print bytes to stdin */
+                printf("%s\n", buf);
+                for (i = 0; i < strlen(buf); i++)
+                    printf("%02x ", buf[i]);
+                printf("\n");
+
+                /* parse client input */
+                if (!strcmp("VERSION\n", buf)) {
+                    // write '1'
+                    r = write(s0, "1\n", 2);
+                }
+                else if (!strcmp("DAYTIME\n", buf)) {
+                    now = time(NULL);
+                    tp = localtime(&now);
+                    strcpy(buf, asctime(tp));
+
+                    r = write(s0, buf, strlen(buf));
+                }
+                else if (!strcmp("RANDOM\n", buf)) {
+                    srandom(time(NULL));
+                    char rnum[3];
+                    rnum[1] = '\n';
+                    rnum[2] = '\0';
+                    rnum[0] = '0' + ((unsigned int) random() % 10);
+                    r = write(s0, rnum, strlen(rnum));
+                }
+                else {
+                    assert(r != -1);
+                    printf("Child %d: read %d bytes\n", pid, r);
+                    if (r == 0)
+                        break;
+                    r = write(s0, buf, r);
+                }
                 assert(r != -1);
                 printf("Child %d: wrote %d bytes\n", pid, r);
             }
